ac8f03a166ddeef91a849832c3bbef263d8678dd
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-05 16:09:48 -0500

    finished file restructuring

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const db = require("./models");
 
 const app = express();
 
+app.use(require('./routes/api-routes.js'));
+app.use(require('./routes/html-routes.js'));
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -20,73 +23,73 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/api/workouts/range', (req, res) => {
-  Workout.find({}).limit(7)
-  .then(dbWorkouts => {
-    res.json(dbWorkouts)
-  })
-  .catch(err => {
-    res.json(err)
-  })
-})
+// app.get('/api/workouts/range', (req, res) => {
+//   Workout.find({}).limit(7)
+//   .then(dbWorkouts => {
+//     res.json(dbWorkouts)
+//   })
+//   .catch(err => {
+//     res.json(err)
+//   })
+// })
 
-app.get('/api/workouts', (req, res) => {
-  Workout.find({})
-  .then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.json(err);
-  });
-});
+// app.get('/api/workouts', (req, res) => {
+//   Workout.find({})
+//   .then(dbWorkout => {
+//     res.json(dbWorkout);
+//   })
+//   .catch(err => {
+//     res.json(err);
+//   });
+// });
 
-app.post("/api/workouts", function(req, res) {
-  Workout.create({})
-    .then(function(dbWorkout) {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+// app.post("/api/workouts", function(req, res) {
+//   Workout.create({})
+//     .then(function(dbWorkout) {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
 
-});
+// });
 
-app.put("/api/workouts/:id", ({ body, params}, res) => {
-  Workout.findByIdAndUpdate(
-    params.id,
-    {$push: {exercises: body}},
-    {new: true, runValidators: true}
-  )
-  .then(workouts => {
-    res.json(workouts);
-  })
-  .catch(err => {
-    res.json(err);
-  });
-});
+// app.put("/api/workouts/:id", ({ body, params}, res) => {
+//   Workout.findByIdAndUpdate(
+//     params.id,
+//     {$push: {exercises: body}},
+//     {new: true, runValidators: true}
+//   )
+//   .then(workouts => {
+//     res.json(workouts);
+//   })
+//   .catch(err => {
+//     res.json(err);
+//   });
+// });
 
-app.delete('/api/workouts/', ({body}, res) => {
-  Workout.findByIdAndDelete(body.id)
-    .then(() => {
-      res.json(true);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// app.delete('/api/workouts/', ({body}, res) => {
+//   Workout.findByIdAndDelete(body.id)
+//     .then(() => {
+//       res.json(true);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
-// HTML Routes
-app.get('/exercise', function(req, res) {
-  res.sendFile(path.join(__dirname, './public/exercise.html'))
-})
+// // HTML Routes
+// app.get('/exercise', function(req, res) {
+//   res.sendFile(path.join(__dirname, './public/exercise.html'))
+// })
 
-app.get('/stats', function(req, res) {
-  res.sendFile(path.join(__dirname, './public/stats.html'))
-})
+// app.get('/stats', function(req, res) {
+//   res.sendFile(path.join(__dirname, './public/stats.html'))
+// })
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, './public/index.html'))
+// })
 
 
 
